9b1dbed8785d615f2056f7af267a06d0f7afd108
sayyedsiddique <sayyedsiddique055@gmail.com>
2024-12-03 21:25:32 +0530

    second timer function implemented

--- javascript.js
+++ javascript.js
@@ -1,23 +1,50 @@
-(function(){
+(function () {
+  const hoursInput = document.querySelector(".hours");
+  const minutesInput = document.querySelector(".minutes");
+  const secondInput = document.querySelector(".second");
+
+  const startBtnElem = document.querySelector(".startBtn");
+  const stopBtnElem = document.querySelector(".stopBtn");
+  const resetBtnElem = document.querySelector(".resetBtn");
+
+  let countdownTimer = null;
+
+  startBtnElem.addEventListener("click", (e) => {
+    console.log("hoursInput.value ", hoursInput.value);
+    if (
+      hoursInput.value == 0 &&
+      minutesInput.value == 0 &&
+      secondInput.value == 0
+    )
+      return;
+
+    function startInterval() {
+      startBtnElem.style.display = "none";
+      stopBtnElem.style.display = "initial";
+
+      countdownTimer = setInterval(() => {
+        timer();
+      }, 1000);
+    }
+    startInterval();
+  });
+
+  function stopInterval() {}
+
+  function timer() {
+    if (
+      hoursInput.value == 0 &&
+      minutesInput.value == 0 &&
+      secondInput.value == 0
+    ) {
+      hoursInput.value = "";
+      minutesInput.value = "";
+      secondInput.value = "";
+      stopInterval();
+    }else if(secondInput.value != 0){
+        secondInput.value = `${secondInput.value <= 10 ? "0": ""}${secondInput.value - 1}`
+    }
 
 
-    const hoursInput = document.querySelector(".hours")
-    const minutesInput = document.querySelector(".minutes")
-    const secondInput = document.querySelector(".second")
-
-    const startBtnElem = document.querySelector(".startBtn")
-    const resetBtnElem = document.querySelector(".resetBtn")
-
-    startBtnElem.addEventListener("click", (e) => {
-        console.log("Chala", e.target.classList[1])
-        if(e.target.classList[1] === "stop"){
-            startBtnElem.classList.remove("stop")
-            startBtnElem.textContent = "Start"
-        }else{
-            startBtnElem.classList.add("stop")
-            startBtnElem.textContent = "Stop"
-        }
-    })
-
-
-})()
+  }
+})();
